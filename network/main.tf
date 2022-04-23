4f2bb65b6355f9055193aa9b452ec6d595c5d677
infirmux <petran@live.ru>
2022-04-23 15:19:37 +0200

    update

--- network/main.tf
+++ network/main.tf
@@ -98,3 +98,152 @@ resource "aws_route_table_association" "private_rt" {
   subnet_id      = element(aws_subnet.cloudx_private_subnets.*.id, count.index)
   route_table_id = aws_route_table.private_rt.id
 }
+
+###SECURITY GROUPS###
+##1.ec2_pool
+resource "aws_security_group" "ec2_pool" {
+  vpc_id = aws_vpc.cloudx_vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.cloudx_vpc.cidr_block]
+  }
+  ingress {
+    from_port   = 2368
+    to_port     = 2368
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ec2_pool"
+    Description = "allows access for ec2 instances"
+  }
+}
+##2.fargate_pool
+resource "aws_security_group" "fargate_pool" {
+  vpc_id = aws_vpc.cloudx_vpc.id
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.cloudx_vpc.cidr_block]
+  }
+  ingress {
+    from_port   = 2368
+    to_port     = 2368
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "fargate_pool"
+    Description = "allows access for fargate instances"
+  }
+}
+##3.mysql
+resource "aws_security_group" "mysql" {
+  vpc_id = aws_vpc.cloudx_vpc.id
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.ec2_pool.id]
+  }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.fargate_pool.id]
+  }
+  tags = {
+    Name = "mysql"
+    Description = "defines access to ghost db"
+  }
+}
+##4.efs
+resource "aws_security_group" "efs" {
+  vpc_id = aws_vpc.cloudx_vpc.id
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    security_groups = [aws_security_group.ec2_pool.id]
+  }
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    security_groups = [aws_security_group.fargate_pool.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.cloudx_vpc.cidr_block]
+  }
+  tags = {
+    Name = "efs"
+    Description = "defines access to efs mount points"
+  }
+}
+##5.alb
+resource "aws_security_group" "alb" {
+  vpc_id = aws_vpc.cloudx_vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+#    security_groups = [aws_security_group.ec2_pool.id]
+    cidr_blocks = [aws_vpc.cloudx_vpc.cidr_block]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+#    security_groups = [aws_security_group.fargate_pool.id]
+    cidr_blocks = [aws_vpc.cloudx_vpc.cidr_block]
+  }
+  tags = {
+    Name = "alb"
+    Description = "defines access to alb"
+  }
+}
+
+##6.vpc_endpoint
+resource "aws_security_group" "vpc_endpoint" {
+  vpc_id = aws_vpc.cloudx_vpc.id
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.cloudx_vpc.cidr_block]
+  }
+  tags = {
+    Name = "vpc_endpoint"
+    Description = "defines access to vpc endpoints"
+  }
+}
